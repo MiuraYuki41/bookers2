603a30f7470ded6053906fb520c74b01e47f4c02
MiuraYuki41 <205943986+MiuraYuki41@users.noreply.github.com>
2025-07-16 17:15:42 +0000

    commit4

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
- get 'users/show'
- get 'users/edit'
- get 'books/new'
- get 'books/index'
- get 'books/show'
- get 'books/edit'
- # get 'homes/top'
+ #get 'users/show'
+ #get 'users/edit'
+ #get 'books/new'
+ #get 'books/index'
+ #get 'books/show'
+ #get 'books/edit'
 
-  root to: "homes#top"
-  devise_for :users
+ root to: "homes#top"
+ devise_for :users
+
+ get '/homes/about' => 'homes#about'
+
+ resources :books, only: [:new, :index, :show, :edit]
+ resources :users, only: [:new, :index, :show, :edit]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
